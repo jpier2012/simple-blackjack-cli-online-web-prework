f51ef9e4dd8e0bd185753b9ddebb9e8ad87554a1
James Pier <jpier2012@gmail.com>
2019-04-01 16:38:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,7 +40,7 @@ def hit? card_total
   prompt_user
   input = get_user_input
   if input == "s"
-    card_total  
+    card_total
   elsif input == "h"
     card_total = card_total + deal_card
   else
