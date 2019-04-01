02717ef02b6dec4b822b7e9cdb04f60a57cb5a32
James Pier <jpier2012@gmail.com>
2019-04-01 17:07:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,5 +66,17 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  card_total = 0
+  welcome
+  card_total = initial_round
+  until card_total >= 21
+    card_total = hit? card_total
+    display_card_total
+    if card_total > 21
+      end_game
+    elsif card_total == 21
+      puts "You win!!"
+    end
+  end
 end
+
