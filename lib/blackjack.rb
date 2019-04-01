0837e18044cb89d88156b658f00bbf74bf8dfa4f
James Pier <jpier2012@gmail.com>
2019-04-01 17:12:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,11 +61,11 @@ def runner
   card_total = initial_round
   until card_total >= 21
     card_total = hit? card_total
+    display_card_total card_total
     if card_total > 21
       end_game card_total
     elsif card_total == 21
       puts "You win!!"
     end
-    display_card_total card_total
   end
 end
