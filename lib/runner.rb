57f5d3029aa4c06a63502499ad2b19d1b33a0c6b
James Pier <jpier2012@gmail.com>
2019-04-01 17:01:20 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -3,7 +3,7 @@ require_relative "blackjack.rb"
 def runner
   card_total = 0
   welcome
-  initial_round
+  card_total = initial_round
   until card_total >= 21
     hit? card_total
     if card_total > 21
