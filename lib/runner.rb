048b9d1526b2630444887ffa380e9134f3f0ac11
James Pier <jpier2012@gmail.com>
2019-04-01 17:10:40 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,18 +1,3 @@
 require_relative "blackjack.rb"
 
-def runner
-  card_total = 0
-  welcome
-  card_total = initial_round
-  until card_total >= 21
-    card_total = hit? card_total
-    display_card_total
-    if card_total > 21
-      end_game
-    elsif card_total == 21
-      puts "You win!!"
-    end
-  end
-end
-
 runner
