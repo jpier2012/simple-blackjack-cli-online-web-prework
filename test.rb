a9eec9d43237eb7fb2b1685cbc31ac88561917af
James Pier <jpier2012@gmail.com>
2019-04-01 15:18:04 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,7 +1,9 @@
-def OuterClass
-  def InnerClass
-    name = "James"
-  end
+MR_COUNT = 0        # constant defined on main Object class
+module Foo
+  MR_COUNT = 0
+  ::MR_COUNT = 1    # set global count to 1
+  MR_COUNT = 2      # set local count to 2
 end
 
-puts OuterClass::InnerClass::name
+puts MR_COUNT       # this is the global constant
+puts Foo::MR_COUNT  # this is the local "Foo" constant
